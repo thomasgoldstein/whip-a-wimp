05d2526e68e2242bd843645cf767bfa90a7c79b1
Don Miguel <mikhail.bratus@gmail.com>
2013-11-15 16:45:10 +0600

    * Fixed swapped x and y coords in the functions (we had no bugs due to the level 9x9 symmetry) + added bigger debug level print to see connections between rooms properly + added correct connections between rooms into the level generator

--- src/level.js
+++ src/level.js
@@ -48,17 +48,42 @@ rooms.genLevel = function() {
 	//1st room is at 4,4 pas always
 	var x = 4;
 	var y = 4;
+	var oldx = x;
+	var oldy = y;
 	var noCycle = 0;
 	do {
 		noCycle++;
-		if(!rooms[x][y]) {
+		if(!rooms[y][x]) {
 			var r = new room("Room "+x+"-"+y,x,y);
-			rooms[x][y] = r;
+			if(x == 4 && y == 4){ //for TEST mark start room
+				//r.walls.up = r.walls.down = r.walls.left = r.walls.right = "empty";
+				r.walls.bottom = "start";
+			}
+			//make connection between previous and current rooms
+			if(oldx < x){
+				rooms[oldy][oldx].walls.right = "empty";
+				r.walls.left = "empty";
+			}
+			if(oldx > x){
+				rooms[oldy][oldx].walls.left = "empty";
+				r.walls.right = "empty";
+			}
+			if(oldy < y){
+				rooms[oldy][oldx].walls.down = "empty";
+				r.walls.up = "empty";
+			}
+			if(oldy > y){
+				rooms[oldy][oldx].walls.up = "empty";
+				r.walls.down = "empty";
+			}
+			rooms[y][x] = r;
 		} else {
 			//cannot put a room here
 			noCycle--;
 		}
 
+		oldx = x;
+		oldy = y;
 		// % CHANCE
 		if(Math.random()*100 < 50) {
 			//X
@@ -94,24 +119,92 @@ rooms.genLevel = function() {
 		//check for max number of rooms to generate
 	} while(noCycle<15 );
 	//r.walls.up = "door";
+
+	//room - marker 0,0
+	x = 0; y = 0;
+	r = new room("Room "+x+"-"+y,x,y);
+	r.walls.down = r.walls.right = "empty";
+	rooms[y][x] = r;
+
+	//room - marker 8,8
+	x = 8; y = 8;
+	r = new room("Room "+x+"-"+y,x,y);
+	r.walls.up = r.walls.left = "empty";	
+	rooms[y][x] = r;
 }
 
-rooms.printLevel = function() {
-	//print debug level map
+//print 1 of 3 lines of the set room 4 debug
+// #|#  ###
+// - -  # #  .
+// #|#  ###
+function getLineRoom(room, line){
+	if(!room)
+		if(line == 1)
+			return " . ";
+		else
+			return "   ";
+	switch(line){
+	case 0:	//up
+		if(room.walls.up == "wall")
+			return "###";
+		else
+			return "#|#";
+	case 1:	//left + right
+		return ((room.walls.left == "wall")?"#":"-")+((room.walls.bottom == "start")?"S":" ")+((room.walls.right == "wall")?"#":"-");
+	case 2:	//down
+		if(room.walls.down == "wall")
+			return "##W";
+		else
+			return "#|#"; // i return different line, because FireBug does shorten the outpt. If I output 2 equal lines, it prints only 1 instead
+	default:
+		return "???";
+	}
+}
+
+//Small
+rooms.printSLevel = function() {
+	//print Small debug level map
 	for(var y = 0; y < 9 ; y++) {
-		var s =""
-		for(var x = 0; x < 9 ; x++) {
-			if(rooms[y][x])
-				s = s + rooms[y][x].name[0];
-			else
-				s = s + "#";
+		var s ="";
+		for(var x = 0; x < 9; x++) {
+			var r = rooms[y][x];
+			if(r)	//is it a start room?
+				if(r.walls.bottom == "start")
+					s = s + "S";
+				else
+					s = s + "W";
+				//s = s + r.name[0];
+			else	//wall
+				s = s + ".";
 		}
 		console.log(s);
 	}
 }
 
+//BIG
+rooms.printLevel = function() {
+	//print debug level map
+	for(var y = 0; y < 9 ; y++) {
+		for(var i=0; i<3; i++) {
+			var s ="";
+			for(var x = 0; x < 9; x++) {
+				s = s + getLineRoom(rooms[y][x], i);
+				//if(rooms[y][x])
+				//	s = s + rooms[y][x].name[0];
+				//else
+				//	s = s + "#";
+			}
+			console.log(s);
+		}
+	}
+}
+
+
 rooms.initLevel();
 rooms.genLevel();
+console.info("Small Level");
+rooms.printSLevel();
+console.info("Big Level");
 rooms.printLevel();
 
 console.log(rooms);
