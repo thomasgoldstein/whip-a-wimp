98c949567fd74bceffa5b6ce07612aaf0526eb6c
Don Miguel <mikhail.bratus@gmail.com>
2014-04-01 17:24:20 +0600

    Added Bouncin pattern to enemy

--- src/enemy.js
+++ src/enemy.js
@@ -9,8 +9,8 @@ waw.Enemy = waw.Unit.extend({
     speed: 1,
     movement: null,
     direction: null,
-//    dx: 1,
-//    dy: -1,
+    dx: 1,
+    dy: -1,
     targetX: 160,
     targetY: 110,
     safePos: null,
@@ -27,6 +27,7 @@ waw.Enemy = waw.Unit.extend({
 
         this.SCHEDULE_IDLE = new waw.Schedule([this.initIdle, this.onIdle], ["seeEnemy"]);
         this.SCHEDULE_WALK = new waw.Schedule([this.initWalk, this.onWalk], ["feelObstacle"]);
+        this.SCHEDULE_BOUNCE = new waw.Schedule([this.initBounce, this.onBounce], ["feelObstacle"]);
 
         this.setContentSize(16, 16);
         this.setAnchorPoint(0, -1);
@@ -69,13 +70,18 @@ waw.Enemy = waw.Unit.extend({
         conditions.push("canWalk"); //always possible
         return conditions;
     },
-    pickAISchedule: function(){
+    pickAISchedule: function () {
         switch (this.state) {
             case "idle":
                 if (Math.random() < 0.7) {
                     this.state = "idle";
                     this.stateSchedule = this.SCHEDULE_IDLE;
 //                    console.log("-idle");
+                } else if (Math.random() < 0.3) {
+                    this.state = "bounce";
+                    this.stateSchedule = this.SCHEDULE_BOUNCE;
+//                    console.log("-idle");
+
                 } else {
                     this.state = "walk";
                     this.stateSchedule = this.SCHEDULE_WALK;
@@ -90,6 +96,17 @@ waw.Enemy = waw.Unit.extend({
                 } else {
                     this.state = "walk";
                     this.stateSchedule = this.SCHEDULE_WALK;
+//                    console.log("-walk");
+                }
+                break;
+            case "bounce":
+                if (Math.random() < 0.3) {
+                    this.state = "idle";
+                    this.stateSchedule = this.SCHEDULE_IDLE;
+//                    console.log("-idle");
+                } else {
+                    this.state = "bounce";
+                    this.stateSchedule = this.SCHEDULE_BOUNCE;
 //                    console.log("-walk");
                 }
                 break;
@@ -132,10 +149,10 @@ waw.Enemy = waw.Unit.extend({
             x = pos.x;
             y = pos.y;
         }
-        this.setPosition(x, y);
+        this.setPosition(x, y-1);
         this.setZOrder(250 - y);
         //position shadow
-        this.shadowSprite.setPosition(pos.x, pos.y-6);
+        this.shadowSprite.setPosition(pos.x, pos.y+2);
         return true;
     },
     onIdle: function () {
@@ -194,7 +211,60 @@ waw.Enemy = waw.Unit.extend({
         this.setPosition(x, y);
         this.setZOrder(250 - y);
         //position shadow
-        this.shadowSprite.setPosition(pos.x, pos.y-6);
+        this.shadowSprite.setPosition(pos.x, pos.y-0);
+
+        if (cc.pDistanceSQ(cc.p(this.targetX, this.targetY), pos) < 32) {
+            return true; //get to the target x,y
+        }
+        return false;
+    },
+    initBounce: function () {
+        var currentTime = new Date();
+        this.timeToThink = currentTime.getTime() + 2500 + Math.random() * 5500;
+        if (this.dx == 0 || this.dy == 0) {
+            //random way to bounce
+            this.dx = Math.round(1 - Math.random()*2);
+            this.dy = Math.round(1 - Math.random()*2);
+        } else {
+            this.dx = -this.dx;
+            this.dy = -this.dy;
+        }
+        return true;
+    },
+    onBounce: function () {
+        var currentTime = new Date();
+        if (currentTime.getTime() > this.timeToThink) {
+            return true;
+        }
+
+        var pos = this.getPositionF(),
+            oldPos = pos,
+            x = pos.x,
+            y = pos.y;
+
+        this.oldPos = pos;
+
+        var d = cc.Director.getInstance();
+        var fps = d.getAnimationInterval();
+        var speed = this.speed * fps * 10;
+
+        //try to move unit
+            x += this.dx*speed;
+            y += this.dy*speed;
+
+        if(x<50 || x>270) {
+            this.dx = -this.dx;
+//            x = oldPos.x;
+        }
+        if(y<40 || y>180) {
+            this.dy = -this.dy;
+//            y = oldPos.y;
+        }
+
+        this.setPosition(x, y);
+        this.setZOrder(250 - y);
+        //position shadow
+        this.shadowSprite.setPosition(pos.x, pos.y-0);
 
         if (cc.pDistanceSQ(cc.p(this.targetX, this.targetY), pos) < 32) {
             return true; //get to the target x,y
