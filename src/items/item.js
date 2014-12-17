e6490f15d06b0064032ef3d899bb0211114a3dda
Don Miguel <mikhail.bratus@gmail.com>
2014-12-17 23:18:42 +0500

    cleanup of the source

--- src/items/item.js
+++ src/items/item.js
@@ -17,20 +17,20 @@ waw.Item = waw.Unit.extend({
         //console.info("Item ctor "+itemT);
         this.itemType = itemT;
         this.setContentSize(8, 8);
-        switch(this.itemType){
+        switch (this.itemType) {
             case "key":
-                this.sprite = new cc.Sprite(s_Items, cc.rect(0,0,16,16));
+                this.sprite = new cc.Sprite(s_Items, cc.rect(0, 0, 16, 16));
                 break;
             case "coin":
-                this.sprite = new cc.Sprite(s_Items, cc.rect(17*1,0,16,16));
+                this.sprite = new cc.Sprite(s_Items, cc.rect(17 * 1, 0, 16, 16));
                 break;
             case "gem":
-                this.sprite = new cc.Sprite(s_Items, cc.rect(17*2,0,16,16));
+                this.sprite = new cc.Sprite(s_Items, cc.rect(17 * 2, 0, 16, 16));
                 break;
             default:
-                this.sprite = new cc.Sprite(s_Items, cc.rect(17*(3+Math.round(Math.random()*1)),0,16,16));
+                this.sprite = new cc.Sprite(s_Items, cc.rect(17 * (3 + Math.round(Math.random() * 1)), 0, 16, 16));
         }
-        this.sprite.setPosition(0,this.spriteYoffset); //pig 48x48
+        this.sprite.setPosition(0, this.spriteYoffset); //pig 48x48
         this.sprite.setAnchorPoint(0.5, 0);
         this.addChild(this.sprite, 0, TAG_SPRITE);
 
@@ -46,7 +46,7 @@ waw.Item = waw.Unit.extend({
 
         this.scheduleUpdate();
     },
-    cleanUp: function() {
+    cleanUp: function () {
         var i;
         this.unscheduleUpdate();
 
@@ -60,7 +60,6 @@ waw.Item = waw.Unit.extend({
             ),
             new cc.RemoveSelf()
         ));
-        //this.getParent().removeChild(this.shadowSprite);  //remove shadow
         this.shadowSprite.runAction(new cc.Sequence(
             new cc.Spawn(
                 new cc.FadeOut(0.2),
@@ -69,32 +68,30 @@ waw.Item = waw.Unit.extend({
             new cc.RemoveSelf()
         ));
 
-        for(i=0; i< waw.items.length; i++){ //remove from current items array
-            if(waw.items[i] === this) {
+        for (i = 0; i < waw.items.length; i++) { //remove from current items array
+            if (waw.items[i] === this) {
                 waw.items[i] = null;
                 currentRoom.items[i] = null;
                 break;
             }
         }
-        //this.removeChild(this.debugCross);
-        //this.removeChild(this.label);
     },
     update: function () {
         //check conditions
         var pPos = waw.player.getPosition();
         var pos = this.getPosition();
         if (cc.pDistanceSQ(pPos, pos) < 200) {
-                this.onTake();
-                this.cleanUp();
+            this.onTake();
+            this.cleanUp();
         }
-        if(showDebugInfo && this.label) {
+        if (showDebugInfo && this.label) {
 //            this.label.setString(""+this.state + " "+ cc.pDistanceSQ(pPos, pos) );
             this.label.setString(this.itemType); //+":"+pos.x.toFixed(2)+","+pos.y.toFixed(2) );
         }
     },
     onTake: function () {
         //player takes item
-        switch(this.itemType){
+        switch (this.itemType) {
             case "key":
                 waw.keys += 1;
                 waw.addScore(10);
@@ -120,5 +117,4 @@ waw.Item = waw.Unit.extend({
     onDestroy: function () {
         //what if the item has been destroyed by some forCe
     }
-})
-;
+});
