db2ee71138251cf080d5dc34a25c1ee668db3741
Don Miguel <mikhail.bratus@gmail.com>
2015-03-10 22:12:27 +0500

    added function waw.SpriteRect to set sprite sheet frames with its columns and rows numbers

--- src/functions.js
+++ src/functions.js
@@ -12,3 +12,10 @@ waw.deepCopy = function(o) {
     return copy;
 };
 
+//prowide width and height of the spritesheet frame
+//and get a function that returns correct frame rect by its COLUMN, ROW
+waw.SpriteRect = function(w, h) {
+    return function(x , y) {
+        return cc.rect(1+(w+2)*x, 1+(h+2)*y, w, h);
+    };
+};
