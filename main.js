8c46b67f72926c6273b4fab86d7909bd80082ebe
Thomas Goldstein <stifu@free.fr>
2015-03-18 19:42:07 +0100

    Automatically resize the game canvas with the browser size

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 "use strict";
 window.onload = function () {
     cc.game.onStart = function () {
-        var designSize = cc.size(320, 240);
-        //var screenSize = cc.view.getFrameSize();
-        cc.view.setDesignResolutionSize(designSize.width, designSize.height, cc.ResolutionPolicy.SHOW_ALL);
-
-        //load resources
-//      cc.LoaderScene.preload(["LoadingScreen.png"], function () {
-        //cc._loaderImage = "data:image/png;base64,iVBORw ... fQAAAABJRU5ErkJggg==";
+        cc.view.adjustViewPort(true);
+        cc.view.setDesignResolutionSize(320, 240, cc.ResolutionPolicy.SHOW_ALL);
+        cc.view.resizeWithBrowserSize(true);
         cc.LoaderScene.preload([g_resources], function () {
             cc.director.runScene(new waw.TitleScene());
         }, this);
