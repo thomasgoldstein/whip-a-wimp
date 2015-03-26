9f85aaea9c7cb4e4ff942271383ff5cc4db0ec56
Don Miguel <mikhail.bratus@gmail.com>
2015-03-26 23:08:12 +0500

    added tiny animation to the "go to next level" scene

--- src/scenes/gotoNextLEvel.js
+++ src/scenes/gotoNextLEvel.js
@@ -10,10 +10,10 @@ waw.gotoNextLevel = cc.Scene.extend({
 
         this.scheduleOnce(function () {
             var transition = cc.TransitionFade;
-            rooms.initLevel();
+
             waw.currentScene = new waw.MainScene();
-            cc.director.runScene(new transition(1,waw.currentScene));
-        }, 3);
+            cc.director.runScene(new transition(3,waw.currentScene));
+        }, 2);
     }
 });
 
@@ -24,9 +24,7 @@ waw.gotoNextLevelLayer = cc.Layer.extend({
         cc.audioEngine.stopAllEffects();
         cc.audioEngine.stopMusic();
 
-        waw.theme.gotoNextLevel();
-
-        var label = new cc.LabelTTF(waw.theme.name+"\nNEXT LEVEL "+(waw.theme.levelN+1), "System", 32);
+        var label = new cc.LabelTTF(waw.theme.name+" "+(waw.theme.levelN+1), "System", 32);
         label.setAnchorPoint(0.5, 0.5);
         this.addChild(label, 299 + 5);
         label.setPosition(320/2, 240/2);
@@ -38,9 +36,27 @@ waw.gotoNextLevelLayer = cc.Layer.extend({
     onEnterTransitionDidFinish: function () {
         this._super();
         console.info("onEnterTransitionDidFinish GO");
+        //load + calc new levels
+        waw.theme.gotoNextLevel();
+        rooms.initLevel();
     },
     onExitTransitionDidStart: function () {
         this._super();
         console.info("onExitTransitionDidStart GO");
+
+        this.addChild(waw.player,10);
+        //waw.player.direction = "right";
+        waw.player.sprite.playAnimation("walk_right");
+        waw.player.sprite2.playAnimation("walk_right");
+
+        //waw.player.scaleTo(1.5, 1.5);
+        waw.player.setPosition(-50,50);
+        waw.player.runAction(
+            new cc.Sequence(
+                new cc.MoveTo(2, 160, 50)
+                //new cc.JumpTo(2, 330, 200, 5)
+                //new cc.RemoveSelf()
+            )
+        );
     }
 });
