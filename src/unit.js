45bbee6ebb114f943072205b9566c159fb064c93
Don Miguel <mikhail.bratus@gmail.com>
2015-04-02 10:29:22 +0500

    optimized CollideRect method

--- src/unit.js
+++ src/unit.js
@@ -36,16 +36,27 @@ waw.Unit = cc.Node.extend({
         return cc.rect(pos.x - this.width / 2, pos.y , this.width, this.height);
     },
     doesCollide: function (_units) {
-        //if(!_units) throw "must be an array in the arg";
+        var cr = this.collideRect();
         for (var unit in _units) {
             if(!unit || !_units[unit] || _units[unit] === this)    //do not compare with self
-            //if(!unit || unit === this)    //do not compare with self
                 continue;
             //TODO this is Better check. but glitches when u enter right room with pressed UP
-            if(cc.rectIntersectsRect(this.collideRect(), _units[unit].collideRect())){
+            if(cc.rectIntersectsRect(cr, _units[unit].collideRect())){
                 return true;
             }
         }
         return false;
+    },
+    getObstacleRect: function (_units) {
+        var cr = this.collideRect();
+        for (var unit in _units) {
+            if(!unit || !_units[unit] || _units[unit] === this)    //do not compare with self
+                continue;
+            //TODO this is Better check. but glitches when u enter right room with pressed UP
+            if(cc.rectIntersectsRect(cr, _units[unit].collideRect())){
+                return _units[unit].collideRect();
+            }
+        }
+        return null;
     }
 });
