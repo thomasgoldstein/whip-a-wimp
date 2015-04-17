f2d9e832c7d465b6107834c4122e8449742adde0
Don Miguel <mikhail.bratus@gmail.com>
2015-04-17 19:15:54 +0500

    fix Game Over scene stopped music

--- src/scenes/gameOver.js
+++ src/scenes/gameOver.js
@@ -21,8 +21,8 @@ waw.GameOverLayer = cc.Layer.extend({
     init: function () {
         this._super();
         console.info("init layer GO");
-        cc.audioEngine.stopAllEffects();
-        cc.audioEngine.stopMusic();
+        //cc.audioEngine.stopAllEffects();
+        //cc.audioEngine.stopMusic();
 
         var label = new cc.LabelTTF("GAME OVER", "System", 32);
         label.setAnchorPoint(0.5, 0.5);
